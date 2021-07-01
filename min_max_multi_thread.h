a33d382d95a68d3a31b8268cd5f76fd1887d00fc
Roman Fihura <romanfigura120@gmail.com>
2021-07-02 00:26:42 +0300

    upt

--- min_max_multi_thread.h
+++ min_max_multi_thread.h
@@ -6,5 +6,6 @@
 #include <vector>
 
 using uint32_tCIt = std::vector<uint32_t>::const_iterator;
+
 void vector_insert(std::vector<uint32_t> &v, uint32_t max, uint32_t amount);
 std::pair<uint32_tCIt, uint32_tCIt> multi_thr(const std::vector<uint32_t> &v);
